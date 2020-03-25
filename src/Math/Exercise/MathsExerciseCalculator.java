b27980e75494bf3030b007d037ef28ef2d86d048
Safwan-Akhtar <safwan.qa@hotmail.com>
2020-03-25 10:37:45 +0000

    firs commit

---
+++ src/Math/Exercise/MathsExerciseCalculator.java
@@ -0,0 +1,13 @@
+package Math.Exercise;
+
+public class MathsExerciseCalculator {
+	double division(double num1, double num2) {
+		return num1 / num2;
+	}
+	int subtract(int num1, int num2) {
+		return num1 - num2;
+	}
+	int multiply(int num1, int num2) {
+		return num1 * num2;
+	}
+}
---
+++ src/Math/Exercise/Runner.java
@@ -0,0 +1,10 @@
+package Math.Exercise;
+
+public class Runner {
+	public static void main(String[] args) {
+		MathsExerciseCalculator calc = new MathsExerciseCalculator();
+			System.out.println(calc.division(3,2));
+			System.out.println(calc.subtract(2,3));
+			System.out.println(calc.multiply(2,3));
+	}
+}
